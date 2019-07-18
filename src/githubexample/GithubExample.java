5c3d5d91f876a5edc623f362194de05c8ff847c6
Yamada Kun <thethongghg@gmail.com>
2019-07-18 20:31:29 +0700

    by thong

---
+++ src/githubexample/GithubExample.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubexample;
+
+/**
+ *
+ * @author The Thong
+ */
+public class GithubExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello All Members in SE1311 !!!");
+    }
+    
+}
